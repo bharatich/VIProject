b2e236671edafd28fe926123372a1a0a7d44fe6b
bharatich <bharati023@gmail.com>
2019-11-06 10:43:01 +0530

    added new project

---
+++ VIProject_IN/src/main/java/VIProject_IN/VIProject_IN/App.java
@@ -0,0 +1,13 @@
+package VIProject_IN.VIProject_IN;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
